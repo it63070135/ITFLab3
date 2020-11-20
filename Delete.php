83eb4bca0eb8a6be261a9b2370609ef8711d9446
it63070135 <73655292+it63070135@users.noreply.github.com>
2020-11-20 20:50:18 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Delete | 13th ITF LAB</title>
+	<title>Delete</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
@@ -14,12 +14,12 @@
 		}
 	</style>
 </head>
-<body class="bg-light py-5">
-	<div class="container text-dark">
+<body class="bg-dark py-5">
+	<div class="container text-light">
 		<div class="row">
 			<div class="col-12 col-lg-8 offset-lg-2">
 				<div class="card shadow">
-					<div class="card-body">
+					<div class="card-body text-dark">
 						<h2 align="center">
 						<?php
 							if(mysqli_query($conn, $sql)) {
